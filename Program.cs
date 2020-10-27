52e78863455b698a9016e7f1aee8cd5c7fe55488
SebEil <56046884+SebEil@users.noreply.github.com>
2020-10-27 15:48:53 +0100

    Test

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PG3302_Eksamen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
