1d47c10ead767f273d68b963367d621c6284cf7c
rehma <rehma@DESKTOP-NMLC9HO>
2020-11-15 01:19:14 +0100

    Finish with GameBoard.cs  , Next ---> Player.cs

--- GameBoard.cs
+++ GameBoard.cs
@@ -42,14 +42,14 @@ namespace PG3302_Eksamen
         private readonly HumanPlayer _player;
         private readonly ComputerPlayer _dealer;
         private readonly Random _cardDeck;
-        
+
         public GameBoard()
         {
             _player = new HumanPlayer(this);
             _dealer = new ComputerPlayer(this);
             _cardDeck = new Random();
-               }
-        
+        }
+
         public void PlayGame()
         {
 
@@ -64,11 +64,41 @@ namespace PG3302_Eksamen
 
                 _dealer.InitialHand();
 
-                if ((_player.CurrentTotal <= 21) && (_player.HasBlackJack()){
+                if ((_player.CurrentTotal <= 21) && (_player.HasBlackJack())){
                     _dealer.PreformTrun();
                 }
                 DisplayEndGameMessage();
             } while (_player.WantsToKeepPlaying());
+
+        }
+        public Card DealCard()
+
+        {
+            int cardValue = _cardDeck.Next(1, 13);
+            return (Card)cardValue;
+
+        }
+
+        private void Reset()
+        {
+            _player.Reset();
+
+            _dealer.Reset();
+
+            Console.Clear();
+        }
+
+        private void DisplayEndMessage() {
+            if (_player.HasBlackJack()) {
+                Console.WriteLine(" \n You Won: You got BlackJack!");
+            } else if (_player.CurrentTotal > 21) {
+                Console.WriteLine(" \n You busted, so you lost.");
+            } else if (_dealer.CurrentTotal > 21) {
+                Console.WriteLine(" \n The dealer busted, so you won! (The drealer's total was {0}.)", _dealer.CurrentTotal);
+            } else if (_player.CurrentTotal > _dealer.CurrentTotal) {
+                Console.WriteLine(" \n You won! (Your   total was {0},the Dealer dealer's total was {1}.)", _player.CurrentTotal);
+            } else {
+                Console.WriteLine(" \n You lost. (Your   total was {0},the Dealer dealer's total was {1}.)", _player.CurrentTotal); }
         }
     }
 }
